1f8d749eeaec21291605e154e68acf41455feac0
Brandon <leebran@onid.oregonstate.edu>
2015-10-19 18:17:35 -0700

    Added Rutger's stuff

--- Group Assignment 2/Group Assignment 2.py
+++ Group Assignment 2/Group Assignment 2.py
@@ -1,3 +1,42 @@
+import numpy as np
+
+arr = np.array([[1, 2, 5],
+                [8, 7, -7],
+                [-3, -1, 6]])
+
+arrHeight = arr.shape[0] - 1
+arrWidth  = arr.shape[1] - 1
+
+results = []
+
+# Takes a "root" and treats every possible option as a tree branch, 
+# returning the best path
+def bestPathFromRoot(x, y, curResult):
+    results.append(curResult)
+    curResult = curResult + arr[y, x]
+    if x > 0:
+        bestPathFromRoot(x-1, y, curResult)
+    if y > 0:
+        bestPathFromRoot(x, y-1, curResult)
+    return max(results)
+
+finalResults = []
+# Iterate through bottom row
+for i in range(0, arrWidth):
+    results = []
+    res = bestPathFromRoot(i, arrHeight, arr[0][0])
+    finalResults.append(res)
+
+# Iterate through right column
+for j in range(0, arrHeight):
+    results = []
+    res = bestPathFromRoot(arrWidth, j, arr[0][0])
+    finalResults.append(res)
+
+print(max(finalResults))
+
+
+'''
 import numpy
 
 print numpy.zeros((5,5))
@@ -52,3 +91,4 @@ def findBestSolution(matrix, startPoint, bestSolution):
 		bestSolution = currentSolution
 
 	return currentSolution
+'''
