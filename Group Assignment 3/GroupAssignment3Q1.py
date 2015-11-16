c22f0d295ba580d3f57086dd73e9a5f259559d4c
Brandon <leebran@onid.oregonstate.edu>
2015-11-15 21:18:24 -0800

    Draft 2 of pseudocode

--- Group Assignment 3/GroupAssignment3Q1.py
+++ Group Assignment 3/GroupAssignment3Q1.py
@@ -1,9 +1,12 @@
-def problem1():
-    maximum = 0
-    for i in range(0, len(arr)):
-        current = 0
-        for j in range(i, len(arr)):
-            current += arr[j]
-            if current > maximum:
-                maximum = current
-    return maximum
+# The following is a pseudocode implementation that isn't expected to properly compile.
+def problem1(arr):
+    mostMagicalSubsequence = []
+    maximumValue = 0
+
+    for i in range(0, n):
+        for j in range(i, n):
+            currentSubsequence = arr[i:j]
+            currentValue = min(currentSubsequence) * sum(currentSubsequence)
+            if currentValue > maximumValue:
+                mostMagicalSubsequence = currentSubsequence
+    return mostMagicalSubsequence
