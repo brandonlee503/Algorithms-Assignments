0c959dbe5de3ba274cb6e8843be3efb868b2ac56
Brandon <leebran@onid.oregonstate.edu>
2015-11-15 22:27:01 -0800

    Fixed off by one issue and added analysis

--- Group Assignment 3/GroupAssignment3Q1.py
+++ Group Assignment 3/GroupAssignment3Q1.py
@@ -5,8 +5,16 @@ def problem1(arr):
 
     for i in range(0, n):
         for j in range(i, n):
-            currentSubsequence = arr[i:j]
+            currentSubsequence = arr[i:j+1]
             currentValue = min(currentSubsequence) * sum(currentSubsequence)
             if currentValue > maximumValue:
                 mostMagicalSubsequence = currentSubsequence
     return mostMagicalSubsequence
+
+# The algorithm problem1() runs in O(n^2) time as it takes in an n size array
+# and checks every element i for a possible most magical subsequence for each
+# length j. In our pseudocode above, this checking is achieved using two for
+# loops (nested), which results in O(n^2) runtime. The subsequence check is
+# implemented by checking the minimum element in the subsequence with the sum of
+# the subsequence. Finally an if statement is used to check for which subsequence
+# has a greater value. This runs in O(1) time. Thus we obtain O(n^2) * O(1) = O(n^2).
